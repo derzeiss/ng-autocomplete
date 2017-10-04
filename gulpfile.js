27e41221dd861017187713bd6b3f9cf09020937c
derzeiss <zeiss_alex@web.de>
2017-10-04 14:39:41 +0200

    [CHANGED] deploy uncompressed js and css to docs

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,19 @@ const babel = require('gulp-babel');
 
 path = {
     src: 'src/',
-    dist: 'dist/'
+    dist: 'dist/',
+    docs: 'docs/'
 };
 
 gulp.task('css', () => {
     return gulp.src(path.src + 'ng-autocomplete.css')
+        // deploy uncompressed
         .pipe(gulp.dest(path.dist))
+
+        // deploy to docs
+        .pipe(gulp.dest(path.docs + 'ng-autocomplete'))
+
+        // deploy minified
         .pipe(rename('ng-autocomplete.min.css'))
         .pipe(cleanCss())
         .pipe(gulp.dest(path.dist))
@@ -26,10 +33,17 @@ gulp.task('js', () => {
     ])
         .pipe(concat('ng-autocomplete.js'))
         .pipe(babel({presets: ['env']}))
+
+        // deploy uncompressed
         .pipe(gulp.dest(path.dist))
+
+        // deploy to docs
+        .pipe(gulp.dest(path.docs + 'ng-autocomplete'))
+
+        // deploy minified
         .pipe(rename('ng-autocomplete.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest(path.dist));
+        .pipe(gulp.dest(path.dist))
 });
 
 gulp.task('build', ['css', 'js']);
